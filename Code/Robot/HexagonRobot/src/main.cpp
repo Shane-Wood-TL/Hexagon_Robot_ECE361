53f3ad2f84fecf0d12758f0bb21cd86e1adfe0fe
Shane Wood <townlake101@gmail.com>
2024-03-23 15:04:09 -0400

    Basic Motor Testing

--- Code/Robot/HexagonRobot/src/main.cpp
+++ Code/Robot/HexagonRobot/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Servo motorA;
+Servo motorC;
+Servo motorB;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(6, OUTPUT); //A0
+  pinMode(36, OUTPUT); //A1
+
+  pinMode(41, OUTPUT); //A0
+  pinMode(42, OUTPUT); //A1
+
+  pinMode(1, OUTPUT); //A0
+  pinMode(2, OUTPUT); //A1
+  //pinMode(15, OUTPUT); //enA
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+  motorA.attach(15);
+  motorB.attach(16);
+  motorC.attach(7);
+  motorA.setPeriodHertz(500);
+  motorB.setPeriodHertz(500);
+  motorC.setPeriodHertz(500);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  digitalWrite(6, LOW);
+  digitalWrite(36,HIGH);
+  motorA.writeMicroseconds(2000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+   digitalWrite(41, LOW);
+  digitalWrite(42,HIGH);
+  motorB.writeMicroseconds(2000);
+
+   digitalWrite(1, LOW);
+  digitalWrite(2,HIGH);
+  motorC.writeMicroseconds(2000);
+  //~790 lowest
+  //2000 (100%) = max
+  //speed barely changes
 }
